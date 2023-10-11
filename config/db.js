78dee8a8948ce6a55fb1d225ea45d50293094cf7
vivek <krvivi28@gmail.com>
2023-10-12 00:05:47 +0530

    chat app v1.0

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const connectToDb = async () => {
   console.log("connection to db...");
   try {
-    await mongoose.connect("mongodb://localhost:27017/chatApp");
+    await mongoose.connect(
+      "mongodb+srv://echo:vivek28@echo.smg3wtt.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("mongodb is connected");
   } catch (error) {
     console.log(`db connection failed with error: ${error}`);
