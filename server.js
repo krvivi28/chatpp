724ab7db5dce4e2149c041f93129d3208477da95
vivek <krvivi28@gmail.com>
2023-11-04 16:41:06 +0530

    changed the port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import server from "./app.js";
 import connectToDb from "./config/db.js";
 
-server.listen(3000, async (err) => {
+server.listen(5000, async (err) => {
   if (err) {
     console.log("server connection failed with error: " + err);
   } else {
     await connectToDb();
-    console.log("server is running at port 3000");
+    console.log("server is running at port 5000");
   }
 });
