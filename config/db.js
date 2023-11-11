46bbe26e4d216cf5fcfd584f1cee8153ab7f9361
vivek <krvivi28@gmail.com>
2023-11-11 15:36:59 +0530

    chaged db to storefleet db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-
+// db for krvivi28 mongodb+srv://echo:vivek28@echo.smg3wtt.mongodb.net/?retryWrites=true&w=majority
+// db for storefeet mongodb+srv://echo:vivek@storefleet.dh4wfxc.mongodb.net/?retryWrites=true&w=majority
 const connectToDb = async () => {
   console.log("connection to db...");
   try {
     await mongoose.connect(
-      "mongodb+srv://echo:vivek28@echo.smg3wtt.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://echo:vivek@storefleet.dh4wfxc.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("mongodb is connected");
   } catch (error) {
